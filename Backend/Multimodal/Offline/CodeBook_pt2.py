22bc3b381520bd9ae76ea50574c867583028155b
Fabricio Lanche <fabricio.lanche@utec.edu.pe>
2026-06-17 11:14:28 -0500

    fix(codebook): actualizar la salida para generar los outputs necesario para la lógica del spimi
    - Revisando mis notas caí en cuenta que la salida anterior solo considerar el mapeo de doc_id y histograma. De modo que aún estaría faltando la lista de token_stream para el procesamiento con SPIMI.
    
    - Se añadió también la salida del codebook ingresado, dado que planeó trabajar con los words_id para indexar las consultas en el b+tree local (de otro modo no sería plauscible trabajar con el centroide de la word visual)

--- Backend/Multimodal/Offline/CodeBook_pt2.py
+++ Backend/Multimodal/Offline/CodeBook_pt2.py
@@ -3,16 +3,22 @@ import numpy as np
 def generate_image_histograms(
     features: list[tuple[int, np.ndarray]],
     codebook: list[np.ndarray],
-) -> list[tuple[int, np.ndarray]]:
+) -> tuple[
+    list[np.ndarray],
+    list[tuple[int, np.ndarray]],
+    list[tuple[int, int]]
+]:
     k_codewords = len(codebook)
+    codebook_np = np.asarray(codebook)
 
     histograms: dict[int, np.ndarray] = {}
+    spimi_stream: list[tuple[int, int]] = []
 
     for image_id, descriptor in features:
         descriptor = np.asarray(descriptor, dtype=np.float32)
 
         dists = np.linalg.norm(
-            np.asarray(codebook) - descriptor,
+            codebook_np - descriptor,
             axis=1,
         )
 
@@ -25,14 +31,20 @@ def generate_image_histograms(
             )
 
         histograms[image_id][codeword] += 1
+        spimi_stream.append((image_id, codeword))
 
-    return list(histograms.items())
+    histograms = list(histograms.items())
+    return codebook, histograms, spimi_stream
 
 def generate_text_histograms(
     features: list[tuple[int, np.ndarray]],
     codebook: list[str],
     vocabulary: np.ndarray,
-) -> list[tuple[int, np.ndarray]]:
+) -> tuple[
+    list[str],
+    list[tuple[int, np.ndarray]],
+    list[tuple[int, int]]
+]:
     vocab_index = {
         word: idx
         for idx, word in enumerate(vocabulary)
@@ -44,10 +56,14 @@ def generate_text_histograms(
     )
 
     histograms: list[tuple[int, np.ndarray]] = []
+    spimi_stream: list[tuple[int, int]] = []
 
     for doc_id, bow in features:
         bow = np.asarray(bow, dtype=np.float32)
         histogram = bow[codebook_idx]
         histograms.append((doc_id, histogram))
 
-    return histograms
+        for idx in codebook_idx:
+            spimi_stream.append((doc_id, idx))
+
+    return codebook, histograms, spimi_stream
