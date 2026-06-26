d37d6110ee289db21f6b27e18ec1d25edc35e7d3
Luciana Yangali <luciana.yangali@utec.edu.pe>
2026-06-26 16:28:29 -0500

    feat(persistence): Actualización para subir datos a la tabla

--- Backend/Multimodal/Offline/Persistence.py
+++ Backend/Multimodal/Offline/Persistence.py
@@ -6,19 +6,18 @@ class PersistenceManager:
     def __init__(self, host="localhost", port="5432", database="multimodal", user="postgres", password="123456"):
         
         self.connection = psycopg2.connect(
-            self,
             host=host,
             port=port,
             database=database,
             user=user,
             password=password
         )
-        
+
         self.cursor = self.connection.cursor()
     
     def create_tables(self):
 
-        self.cur.execute("""
+        self.cursor.execute("""
             CREATE EXTENSION IF NOT EXISTS vector;
 
             DROP TABLE IF EXISTS descriptors;
@@ -34,4 +33,41 @@ class PersistenceManager:
             );
         """)
 
-        self.conn.commit()
+        self.connection.commit()
+
+# hablar con el orquestador para definir los nombres de las columnas
+    def insert_document(self, df):
+
+        rows = [
+            (int(row.doc_id), row.url, row.texto)
+            for row in df.itertuples(index=False)
+        ]
+
+        execute_values(
+            self.cursor,
+            """
+            INSERT INTO descriptors (doc_id, url, texto)
+            VALUES %s
+            ON CONFLICT (doc_id) DO UPDATE SET
+                url = EXCLUDED.url,
+                texto = EXCLUDED.texto;
+            """,
+            rows,
+        )
+
+        self.connection.commit()
+
+    def update_histogram(self, doc_id, histogram):
+        self.cursor.execute(
+            """
+            UPDATE descriptors
+            SET image_histogram = %s
+            WHERE doc_id = %s;
+            """,
+            (histogram, doc_id),
+        )
+        self.connection.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
